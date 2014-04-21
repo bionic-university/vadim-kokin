f245b045584cf96a91ee6a6157857ab1b6669207
kvvn <kvvn@i.kiev.ua>
2014-04-21 11:17:43 +0300

    move index.php to HOMEWORK1 folder

--- HOMEWORK_1/index.php
+++ HOMEWORK_1/index.php
@@ -3,11 +3,11 @@
 $data = $argv[1];
 echo 'Data input: '.$argv[1].PHP_EOL;
 //$data = 'This is a very long text';
+
 if(empty($data)){
-
     echo 'Wrong input data. Script run command is: php index.php \'Your data is here\'';
-
     exit;
 }
+
 $array = str_split($data);
 print_r(array_count_values($array));
